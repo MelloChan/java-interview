991c5abec7b87f1b4e459b364098e44267735f24
MelloYuki <352983198@qq.com>
2017-12-20 23:29:04 +0800

    update 内部类

--- java-exam/src/base/inner/Outside.java
+++ java-exam/src/base/inner/Outside.java
@@ -5,26 +5,38 @@ package base.inner;
  * Created by MelloChan on 2017/12/20.
  */
 public class Outside {
-    private int value;
+    private int outsideValue;
     private static int id;
-    class Inner{
-        private int value;
-        public int value(){return value;}
+
+    class Inner {
+        private int value=1;
+
+        public int value() {
+            System.out.println("outside:"+outsideValue);
+            System.out.println("inner:"+new Inner().value);
+            return value;
+        }
 
         /**
          * 通过 .this 生成外部类对象的引用
+         *
          * @return
          */
-        public Outside outer(){
+        public Outside outer() {
             return Outside.this;
+        }
+
+        public Inner inner() {
+            return build();
         }
     }
 
     /**
      * 通过一个方法来获取内部类对象
+     *
      * @return
      */
-    public Inner build(){
+    public Inner build() {
         return new Inner();
     }
 
@@ -32,13 +44,14 @@ public class Outside {
         /*
         提供一个方法来获取外部类
          */
-        Inner inner=new Outside().build();
+        Inner inner = new Outside().build();
         System.out.println(inner.value());
         /*
          .new 获取内部类对象 前提是需要先new一个外部类对象
          */
-        Outside outside=new Outside();
-        Outside.Inner inner1=outside.new Inner();
+        Outside outside = new Outside();
+        Outside.Inner inner1 = outside.new Inner();
         System.out.println(inner1.outer());
+        System.out.println(inner1.inner());
     }
 }

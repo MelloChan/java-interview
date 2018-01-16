7300bb2a5cc8fda0f1661b2c218610e8d6a622e1
MelloYuki <352983198@qq.com>
2018-01-16 12:07:41 +0800

    update volatile.md

--- java-exam/src/thread/Order.java
+++ java-exam/src/thread/Order.java
@@ -5,16 +5,16 @@ package thread;
  * Created by MelloChan on 2018/1/16.
  */
 public class Order {
-    private static boolean flag;
+//    private static boolean flag;
+    private volatile static boolean flag;
     private static int value;
 
-
     public static void main(String[] args) {
-        new Thread(()->{
-            while (!flag)Thread.yield();
+        new Thread(() -> {
+            while (!flag) Thread.yield();
             System.out.println(value);
         }).start();
-        value=2;
-        flag=true;
+        value = 2;
+        flag = true;
     }
 }

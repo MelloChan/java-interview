6b36dfdbd99565cc4863974b06cdbc54400000f3
MelloYuki <352983198@qq.com>
2018-01-17 08:38:05 +0800

    update reflect.md

--- content/reflect.md
+++ content/reflect.md
@@ -14,12 +14,31 @@
 
 #### 相关api
 
-获取全限定类名
+获取全限定类名:  
 ```
 public static void getClassName() {
         ReflectDemo reflectDemo = new ReflectDemo();
         // 获取 全限定类名  output: base.reflect.ReflectDemo
         System.out.println("output: " + reflectDemo.getClass().getName());
+    }
+```
+
+创建class对象引用:
+```
+public static void classReference() throws ClassNotFoundException {
+        Class[] classes = new Class[3];
+        classes[0] = Class.forName("base.reflect.ReflectDemo");
+        classes[1] = ReflectDemo.class;
+        classes[2] = new ReflectDemo().getClass();
+        for (int i = 0; i < 3; i++) {
+            System.out.println("ClassName : " + classes[i]);
+        }
+        /*
+        output: 
+        ClassName : class base.reflect.ReflectDemo
+        ClassName : class base.reflect.ReflectDemo
+        ClassName : class base.reflect.ReflectDemo
+         */
     }
 ```
 
--- java-exam/src/base/reflect/ReflectDemo.java
+++ java-exam/src/base/reflect/ReflectDemo.java
@@ -5,13 +5,31 @@ package base.reflect;
  * Created by MelloChan on 2018/1/17.
  */
 public class ReflectDemo {
+
     public static void getClassName() {
         ReflectDemo reflectDemo = new ReflectDemo();
         // 获取 全限定类名  output: base.reflect.ReflectDemo
         System.out.println("output: " + reflectDemo.getClass().getName());
     }
 
-    public static void main(String[] args) {
+    public static void classReference() throws ClassNotFoundException {
+        Class[] classes = new Class[3];
+        classes[0] = Class.forName("base.reflect.ReflectDemo");
+        classes[1] = ReflectDemo.class;
+        classes[2] = new ReflectDemo().getClass();
+        for (int i = 0; i < 3; i++) {
+            System.out.println("ClassName : " + classes[i]);
+        }
+        /*
+        output:
+        ClassName : class base.reflect.ReflectDemo
+        ClassName : class base.reflect.ReflectDemo
+        ClassName : class base.reflect.ReflectDemo
+         */
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
         getClassName();
+        classReference();
     }
 }

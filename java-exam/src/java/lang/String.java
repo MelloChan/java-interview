20d7d3883762828b638d470a9548390de1bd26b6
MelloYuki <352983198@qq.com>
2018-01-30 21:28:53 +0800

    format

--- java-exam/src/java/lang/String.java
+++ java-exam/src/java/lang/String.java
@@ -11,7 +11,11 @@ public class String {
     }
 }
 /*
+OutPut:
  错误: 在类 java.lang.String 中找不到 main 方法, 请将 main 方法定义为:
  public static void main(String[] args)
  否则 JavaFX 应用程序类必须扩展javafx.application.Application
+ */
+/*
+如果不用java.lang包名,改为其他合法包名,则 public static void main(java.lang.String[] args){} 需要这样描写才能运行 但并没有什么用.
  */

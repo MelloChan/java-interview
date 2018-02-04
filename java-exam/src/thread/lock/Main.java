9e7cf23eb8e78be061552b6adce711156992c112
MelloYuki <352983198@qq.com>
2018-02-04 22:38:31 +0800

    add

--- java-exam/src/thread/lock/Main.java
+++ java-exam/src/thread/lock/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         SynchronizedDemo demo=new SynchronizedDemo();
         for (int i = 0; i < 10; i++) {
-            new Thread(demo::methodA).start();
+            new Thread(demo::methodB).start();
         }
     }
 }
--- java-exam/src/thread/lock/SynchronizedDemo.java
+++ java-exam/src/thread/lock/SynchronizedDemo.java
@@ -1,13 +1,16 @@
 package thread.lock;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * synchronized demo
+ * synchronized demo  可重入内置锁
  * Created by MelloChan on 2018/2/3.
  */
 public class SynchronizedDemo{
     private final Object object = new Object();
+    private final Lock lock=new ReentrantLock();
 
     public void methodA() {
         synchronized (object) {
@@ -16,6 +19,15 @@ public class SynchronizedDemo{
             doSomething();
             System.out.println("-----END-------");
         }
+    }
+
+    public void methodB() {
+        lock.lock();
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("-----START------");
+        doSomething();
+        System.out.println("-----END-------");
+        lock.unlock();
     }
 
     public static void doSomething() {

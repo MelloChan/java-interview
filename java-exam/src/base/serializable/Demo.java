ac69c2341878043ff99cbda93d5695c088aad49c
MelloYuki <352983198@qq.com>
2018-02-13 21:21:10 +0800

    update 序列化简单例子

---
+++ java-exam/src/base/serializable/Demo.java
@@ -0,0 +1,12 @@
+package base.serializable;
+
+import java.io.Serializable;
+
+/**
+ * Demo 序列化对象
+ * Created by MelloChan on 2018/2/13.
+ */
+public class Demo implements Serializable {
+
+    private static final long serialVersionUID = -6854855447974852853L;
+}
---
+++ java-exam/src/base/serializable/SerializableDemo.java
@@ -0,0 +1,23 @@
+package base.serializable;
+
+import java.io.*;
+
+/**
+ * 序列化简单演示
+ * Created by MelloChan on 2018/2/13.
+ */
+public class SerializableDemo {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        // 序列化至文本文件
+        ObjectOutput out=new ObjectOutputStream(
+                new FileOutputStream("demo.txt")
+        );
+        out.writeObject(new Demo());
+
+        // 反序列化
+        ObjectInput in=new ObjectInputStream(
+                new FileInputStream("demo.txt")
+        );
+        System.out.println(in.readObject().getClass());
+    }
+}

c73194c2a8f35e280f82a632571d3766768a2211
MelloYuki <352983198@qq.com>
2018-02-16 19:57:13 +0800

    update 线程池例子

--- java-exam/src/thread/pools/ThreadPoolDemo.java
+++ java-exam/src/thread/pools/ThreadPoolDemo.java
@@ -1,7 +1,6 @@
 package thread.pools;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * 线程池简单demo
@@ -11,10 +10,12 @@ public class ThreadPoolDemo {
 
     private static ExecutorService service= Executors.newFixedThreadPool(10);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         for (int i = 0; i < 10; i++) {
-            service.execute(new Task());
+//            service.execute(new Task());
+            System.out.println(service.submit(new FutureTask()).get());
         }
+
     }
 
     static class Task implements Runnable{
@@ -22,6 +23,14 @@ public class ThreadPoolDemo {
         @Override
         public void run() {
             System.out.println(Thread.currentThread().getName());
+        }
+    }
+
+    static class FutureTask implements Callable<String>{
+
+        @Override
+        public String call() throws Exception {
+            return Thread.currentThread().getName();
         }
     }
 }

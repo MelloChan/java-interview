534fb9c78ae92dfd141b66524389ea329a521abc
MelloYuki <352983198@qq.com>
2018-02-28 23:54:50 +0800

    update

--- java-exam/src/thread/synchronizer/CountDownLatchDemo.java
+++ java-exam/src/thread/synchronizer/CountDownLatchDemo.java
@@ -1,6 +1,7 @@
 package thread.synchronizer;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 闭锁
@@ -11,27 +12,35 @@ import java.util.concurrent.CountDownLatch;
 public class CountDownLatchDemo {
     static CountDownLatch countDownLatch = new CountDownLatch(2);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(new Task()).start();
         new Thread(new Task()).start();
 
-        new Thread(() -> {
-            try {
-                // 锁为0 唤醒线程
-                countDownLatch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() + "------ 其他后续业务 ------");
-        }).start();
-
-        new Thread(new Task()).start();
-    }
+        System.out.println(Thread.currentThread().getName() + "------ 等待其他线程业务执行完毕 -------");
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName()+" ------- 其他线程业务已执行完毕 ---------");
+        System.out.println(Thread.currentThread().getName()+" -------- 执行main主线程业务 --------");
+    }/* output:
+main------ 等待其他线程业务执行完毕 -------
+Thread-1 ------- 某些业务 -------
+Thread-0 ------- 某些业务 -------
+Thread-1 ------- 业务完毕 -------
+Thread-0 ------- 业务完毕 -------
+main ------- 其他线程业务已执行完毕 ---------
+main -------- 执行main主线程业务 --------
+*/
 
     static class Task implements Runnable {
 
         @Override
         public void run() {
             System.out.println(Thread.currentThread().getName() + " ------- 某些业务 -------");
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " ------- 业务完毕 -------");
             // 锁 -1
             countDownLatch.countDown();
         }

d251ed49d34c6662161cd5765f2ce9a6472189de
MelloYuki <352983198@qq.com>
2018-03-03 00:17:26 +0800

    update

--- java-exam/src/base/features/DefaultMethod.java
+++ java-exam/src/base/features/DefaultMethod.java
@@ -8,12 +8,18 @@ public interface DefaultMethod {
     default long add(long x, long y) {
         return x + y;
     }
+
+    static void doSomething(){
+        System.out.println("--- doSomething ---");
+    }
     static class Main{
         public static void main(String[] args) {
             System.out.println(new DefaultMethod(){
             }.add(100L,200L));
+            DefaultMethod.doSomething();
         }
     }
 }/* output:
 300
+--- doSomething ---
 */

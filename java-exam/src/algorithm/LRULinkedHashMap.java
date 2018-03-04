712d39c828c70c01eded8a71e88b3173fbe613d6
MelloYuki <352983198@qq.com>
2018-03-04 11:31:25 +0800

    add

---
+++ java-exam/src/algorithm/LRULinkedHashMap.java
@@ -0,0 +1,40 @@
+package algorithm;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * LRU算法
+ * Created by MelloChan on 2018/3/4.
+ */
+public class LRULinkedHashMap extends LinkedHashMap {
+    private static final long serialVersionUID = 7912177866579640928L;
+
+    private int capacity;
+
+    LRULinkedHashMap(int capacity){
+        super(16,0.75f,true);
+
+        this.capacity=capacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry eldest) {
+        return size()>capacity;
+    }
+
+    public static void main(String[] args) {
+        Map<Integer,Integer>map=new LRULinkedHashMap(4);
+        map.put(9,3);
+        map.put(7,4);
+        map.put(5,9);
+        map.put(3,4);
+        map.put(6,6);
+        map.put(7,4);
+        map.put(9,3);
+        for (Integer i:
+             map.keySet()) {
+            System.out.println(i);
+        }
+    }
+}

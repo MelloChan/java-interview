40c4f00a3858a041e48ff09329d9561955912706
MelloYuki <352983198@qq.com>
2018-03-14 19:35:43 +0800

    update

---
+++ java-exam/src/algorithms/leetcode/MySqrt.java
@@ -0,0 +1,33 @@
+package algorithms.leetcode;
+
+/**
+ * 求开方(非精确)
+ * 利用二分查找
+ * @author MelloChan
+ * @date 2018/3/14
+ */
+public class MySqrt {
+    public static void main(String[] args) {
+        System.out.println(mySqrt(4));
+        // 2.82842
+        System.out.println(mySqrt(8));
+    }
+    public static int mySqrt(int x) {
+        if(x<=1) {
+            return x;
+        }
+        int low=1,high=x;
+        while (low<=high){
+            int mid=low+(high-low)/2;
+            int sqrt=x/mid;
+            if(sqrt==mid) {
+                return sqrt;
+            } else if(sqrt<mid) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
+    }
+}
